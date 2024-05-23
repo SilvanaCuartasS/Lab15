cf9551ee7c14e8da6dc4e2cd79a957c31c14cab1
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2024-05-23 17:57:08 -0500

    segunda parte

--- utils.js
+++ utils.js
@@ -1,37 +1,36 @@
 export class TodoManager {
-    constructor() {
-      this.todos = this.loadTodos();
-    }
-  
-    loadTodos() {
-      const todos = JSON.parse(localStorage.getItem('todos')) || [];
-      return todos;
-    }
-  
-    saveTodos() {
-      localStorage.setItem('todos', JSON.stringify(this.todos));
-    }
-  
-    addTodo(title) {
-      const newTodo = {
-        id: Date.now(),
-        title,
-        completed: false
-      };
-      this.todos.push(newTodo);
-      this.saveTodos();
-      return newTodo;
-    }
-  
-    toggleTodoCompletion(todoId) {
-      const todo = this.todos.find(t => t.id === todoId);
-      if (todo) {
-        todo.completed = !todo.completed;
-        this.saveTodos();
-      }
-    }
-    getTodos() {
-        return [...this.todos];
-      }
+  constructor() {
+    this.todos = this.loadTodos();
   }
-  
+
+  loadTodos() {
+    const todos = JSON.parse(localStorage.getItem('todos')) || [];
+    return todos;
+  }
+
+  saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  addTodo(title) {
+    const newTodo = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.todos.push(newTodo);
+    this.saveTodos();
+    return newTodo;
+  }
+
+  toggleTodoCompletion(todoId) {
+    const todo = this.todos.find(t => t.id === todoId);
+    if (todo) {
+      todo.completed = !todo.completed;
+      this.saveTodos();
+    }
+  }
+  getTodos() {
+      return [...this.todos];
+    }
+}
